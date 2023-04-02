5688b2bf850be946f7383217443fef419c7ea4fc
dong_yxxn <g0326kdy@gmail.com>
2023-04-02 20:09:20 +0900

    Initial commit

---
+++ src/test/java/com/example/headal_spring_boot/HeadalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headal_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
