bdb6d40c0303d50e53b1ebdaa964744b791f4fa9
dong_yxxn <g0326kdy@gmail.com>
2023-04-30 04:40:50 +0900

    주석 추가

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "index";//앞의 경로와 뒤의 파일 확장자(.mastache) 자동으로 지정
     }
 }
