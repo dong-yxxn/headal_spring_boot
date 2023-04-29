dbf08c5d61db4a1121b750ea7bbc843a280c82a3
dong_yxxn <g0326kdy@gmail.com>
2023-04-30 05:59:30 +0900

    IndexControllerTest INIT

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -1,19 +1,20 @@
 package com.jojoldu.book.springboot.web;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-class IndexControllerTest {
+public class IndexControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;//API Test 최석화 CLASS
