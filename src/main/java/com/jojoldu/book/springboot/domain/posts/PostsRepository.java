bda0d2a06fd5285197235a4d92283221ba1f662d
dong_yxxn <g0326kdy@gmail.com>
2023-05-07 18:11:24 +0900

    JpaRepository 이용시 주의점 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -3,5 +3,7 @@ package com.jojoldu.book.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //JpaRepository<Entity클래스, PK타입> 상속하면 기본적인 CRUD 메소드 자동생성
+//JpaRepository를 통해 Entity 데이터 이용 가능
+//@Repository 표시 안해도 되지만, Entity 클래스와 함께 위치해야됨
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
