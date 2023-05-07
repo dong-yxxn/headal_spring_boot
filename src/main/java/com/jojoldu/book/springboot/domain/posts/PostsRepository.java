70641e5993ebf0f95485f8e3d95f648dbe9ca985
dong_yxxn <g0326kdy@gmail.com>
2023-05-07 17:53:23 +0900

    JpaRepository 상속 이유 주석 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,6 @@ package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+//JpaRepository<Entity클래스, PK타입> 상속하면 기본적인 CRUD 메소드 자동생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
